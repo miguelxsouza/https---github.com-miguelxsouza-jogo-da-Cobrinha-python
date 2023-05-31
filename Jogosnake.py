8616c02c80acb0a09ab2574d19f57c6c22174f66
miguelxsouza <miguelsegunda77@icloud.com>
2023-05-31 12:44:23 -0300

    first commit

---
+++ Jogosnake.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+import random
+
+WINDOWS_SIZE = (600, 600)
+PIXEL_SIZE = 10
+SNAKE_DIRECTION = K_LEFT
+
+def collision(pos1, pos2):
+    return pos1 == pos2
+
+def off_limits(pos1):
+    if 0 <= pos[0] < WINDOWS_SIZE[0] and 0 <= pos[1] < WINDOWS_SIZE[1]:
+        return False
+    else:
+        return True
+ 
+def random_on_grid():
+    x = random.randint(0, WINDOWS_SIZE[0])
+    y = random.randint(0, WINDOWS_SIZE[1])
+    return x // PIXEL_SIZE * PIXEL_SIZE, y // PIXEL_SIZE * PIXEL_SIZE
+
+
+pygame.init()
+screen = pygame.display.set_mode(WINDOWS_SIZE)
+pygame.display.set_caption('snake')
+
+
+snake_pos = [(250, 50), (260, 50), (270, 50)]
+snake_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
+snake_surface.fill((255, 255, 255))
+
+
+apple_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
+apple_surface.fill((255, 0, 0))
+apple_pos = random_on_grid()
+
+snake_direction = SNAKE_DIRECTION
+
+while True:
+    pygame.time.Clock().tick(15)
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == KEYDOWN:
+            if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
+                snake_direction = event.key
+
+    screen.blit(apple_surface, apple_pos)
+
+    if collision(apple_pos, apple_pos[0]):
+        snake_pos.append((-10, -10))
+        apple_pos = random_on_grid()
+         
+    for pos in snake_pos:
+        screen.blit(snake_surface, pos)
+
+    for i in range(len(snake_pos)-1, 0, -1):
+        if collision(snake_pos[0], snake_pos[i]):
+            pygame.quit()
+            quit()
+        snake_pos[i] = snake_pos[i-1]
+
+    if off_limits(snake_pos[0]):
+        pygame.quit()
+        quit()
+
+    if snake_direction == K_UP:
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] - PIXEL_SIZE)
+    elif snake_direction == K_DOWN:
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] + PIXEL_SIZE)
+    elif snake_direction == K_LEFT:
+        snake_pos[0] = (snake_pos[0][0] - PIXEL_SIZE, snake_pos[0][1])
+    elif snake_direction == K_RIGHT:
+        snake_pos[0] = (snake_pos[0][0] + PIXEL_SIZE, snake_pos[0][1])
+
+    pygame.display.flip()
